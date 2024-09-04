bb6fe9385f351624bcbad9122fe83ac4ccb207d7
Ubuntu <ubuntu@ip-172-31-19-206.ec2.internal>
2024-09-04 10:12:51 +0000

    add requirement

---
+++ webui.sh
@@ -0,0 +1,12 @@
+# Update
+sudo apt update -y
+# Install Python3 and Pip
+sudo apt install python3.12 -y
+sudo apt install python3.12-venv -y
+# Create python virtual environment
+python3 -m venv env
+source env/bin/activate
+#Install the required packages
+pip install -r requirements.txt
+#start webui
+python webui2.py
