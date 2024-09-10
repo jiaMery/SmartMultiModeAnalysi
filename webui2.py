c801767e3d6d42e55739ef3dfcb7bf1ab9580707
haozhiyu <48579484+haozhiyu@users.noreply.github.com>
2024-09-10 14:57:38 +0800

    Update webui2.py

--- webui2.py
+++ webui2.py
@@ -793,19 +793,23 @@ def save_frame_to_s3(frame):
 
 
 #摄像头截图分析
-def fn_screenshot_analysis(shotcut_video):
+def fn_screenshot_analysis(image: np.ndarray) -> np.ndarray:
     
-    print(type(shotcut_video))
+    
+    # 将NumPy数组编码为JPEG格式
+    _, jpeg_data = cv2.imencode('.jpg', image)
+ 
+    print(type(jpeg_data))
      # 调用 AWS 服务进行分析
-    face_detection = rekognition.detect_faces(Image={'Bytes': shotcut_video})
-    object_detection = rekognition.detect_labels(Image={'Bytes': shotcut_video})
-    text_detection = rekognition.detect_text(Image={'Bytes': shotcut_video})
+    face_detection = rekognition.detect_faces(Image={'Bytes': jpeg_data.tobytes()})
+    object_detection = rekognition.detect_labels(Image={'Bytes': jpeg_data.tobytes()})
+    text_detection = rekognition.detect_text(Image={'Bytes': jpeg_data.tobytes()})
     #moderated_content = rekognition.detect_moderation_labels(Image={'Bytes': shotcut_video)
     print(face_detection)
     
     # 在图像上绘制边界框
     annotated_frame = draw_bounding_boxes(
-        shotcut_video, 
+        jpeg_data.tobytes(), 
         face_detection['FaceDetails'], 
         object_detection['Labels']
         )
@@ -818,7 +822,9 @@ def fn_screenshot_analysis(shotcut_video):
     with open(local_frame_path, 'wb') as f:
         f.write(annotated_frame)
     
-    return analyzied_snapshot_name
+    #返回带红框截图
+    #return analyzied_snapshot_name
+    return object_detection
 ##################start 界面构建##################
 
 # 示例图片
@@ -930,7 +936,7 @@ with gradio.Blocks() as demo:
         
         # 绑定按钮功能
         fn_open_analysis_btn.click(fn=fn_open_analysis,inputs=[before_video],outputs=[after_video])
-        fn_shutcut_analysis_btn.click(fn=fn_screenshot_analysis,inputs=[shotcut_video],outputs=[video_smart_analysis_result_text,notification_text])
+        fn_shutcut_analysis_btn.click(fn=fn_screenshot_analysis,inputs=[shotcut_video],outputs=[video_smart_analysis_result_text])
         
         
         fn_screenshot_btn.click(fn=fn_screenshot,inputs=[before_video],outputs=[shotcut_video,before_img])
